0bd0481310b7a67fda6d134e52e6d725133205c6
Harbinder Singh <harbindersingh621@gmail.com>
2026-03-13 00:04:08 +0530

    client struct added to solve slow client

--- go-user-service/internal/ws/client.go
+++ go-user-service/internal/ws/client.go
@@ -12,11 +12,27 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func NewClient(conn *websocket.Conn) *Client {
+	return &Client{
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+}
+
 func WsServer(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("upgrade error:", err)
 		return
 	}
-	hub.AddClient <- conn
+	client := NewClient(conn)
+	hub.AddClient <- client
+
+	//Todo: replace with real logger later
+	log.Println("Client successfully added")
 }
